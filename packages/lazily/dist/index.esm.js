b3673d5913bd8fe6c7e0d04a88a83bab75ab0c97
y1j2x34 <y1j2x34@qq.com>
2026-01-22 23:45:47 +0800

    chore: ignore recursive dist directories and remove build artifacts

--- packages/lazily/dist/index.esm.js
+++
@@ -1,9 +0,0 @@
-function helloWorld(name) {
-  if (name) {
-    return `Hello, ${name}!`;
-  }
-  return "Hello, World!";
-}
-
-export { helloWorld };
-//# sourceMappingURL=index.esm.js.map
--- packages/lazily/dist/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Hello World function
- * @param name - Optional name to greet
- * @returns A greeting message
- */
-export declare function helloWorld(name?: string): string;
-//# sourceMappingURL=index.d.ts.map
