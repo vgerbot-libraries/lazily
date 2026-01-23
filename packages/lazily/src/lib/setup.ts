c08c8a239fc16c0099a6f7d08d7e55d956aba89c
y1j2x34 <y1j2x34@qq.com>
2026-01-23 18:32:30 +0800

    feat(lazily): add setup utility for automatic lazy instance resolution in object properties

---
+++ packages/lazily/src/lib/setup.ts
@@ -0,0 +1,16 @@
+import { isLazilyInstance } from "../core/lazily-instance";
+import { onInitialized } from "./onInitialized";
+
+export function setup<T extends object>(object: T, callback: (this: T) => void) {
+    const proxy = new Proxy(object, {
+        set(target, p, newValue, receiver) {
+            if (isLazilyInstance(newValue)) {
+                onInitialized(newValue, (realInstance) => {
+                    Reflect.set(target, p, realInstance, receiver);
+                });
+            }
+            return Reflect.set(target, p, newValue, receiver);
+        },
+    });
+    callback.call(proxy);
+}
