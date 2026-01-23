3c0199cd04f2394edc0f559bb79225d45ca946ad
y1j2x34 <y1j2x34@qq.com>
2026-01-24 00:25:03 +0800

    chore: migrate to rollup-plugin-dts for type generation

---
+++ packages/lazily/rollup.config.dts.ts
@@ -0,0 +1,24 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'rollup';
+import dts from 'rollup-plugin-dts';
+
+const currentFile = fileURLToPath(import.meta.url);
+const currentDir = dirname(currentFile);
+
+export default defineConfig({
+    input: join(currentDir, 'dist/src/index.d.ts'),
+    output: [
+        {
+            file: join(currentDir, 'dist/index.d.ts'),
+            format: 'esm',
+        }
+    ],
+    plugins: [
+        dts({
+            tsconfig: join(currentDir, 'tsconfig.dts.json'),
+        }),
+    ],
+    external: [],
+});
+
